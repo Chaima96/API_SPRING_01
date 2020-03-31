83a4de09eea50c1a7a658bd156f7df13fa1958c8
Lucas Chaima <lucaschaima96@gmail.com>
2020-03-31 07:10:43 -0700

    Initial commit

---
+++ src/main/java/com/springofanhella/ApiSpring01Application.java
@@ -0,0 +1,13 @@
+package com.springofanhella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpring01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpring01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springofanhella/ApiSpring01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springofanhella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpring01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
