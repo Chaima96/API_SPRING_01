2be0fbefaa8713cc460b2104af3e168ad5401b78
Lucas Chaima <lucaschaima96@gmail.com>
2020-04-28 07:10:51 -0700

    Criacao do Hash Test

---
+++ src/test/java/com/springofanhella/service/util/HashUtilTest.java
@@ -0,0 +1,21 @@
+package com.springofanhella.service.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.springofanhella.servicos.util.HashUtil;
+
+@RunWith(SpringRunner.class)
+public class HashUtilTest {
+
+	@Test
+	public void getSecureHashTest() {
+		
+		String hash  = HashUtil.getSecureHash("123");
+		System.out.println(hash);
+		assertThat(hash.length()).isEqualTo(64);
+	}
+}
