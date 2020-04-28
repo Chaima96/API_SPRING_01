05ef3b34c3d5d9a6843ac66a51c2e4e44ac90c01
Lucas Chaima <lucaschaima96@gmail.com>
2020-04-28 07:09:47 -0700

    Criacao do Servico do Usuario

---
+++ src/main/java/com/springofanhella/servicos/ServicoUsuario.java
@@ -0,0 +1,57 @@
+package com.springofanhella.servicos;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springofanhella.domain.Usuario;
+import com.springofanhella.repositorio.RepositorioUsuario;
+import com.springofanhella.servicos.util.HashUtil;
+
+@Service
+public class ServicoUsuario {
+
+	/**
+	 * A classe servico sera responsavel 
+	 * por implementas os metodos da interface 
+	 * repositor
+	 */
+	
+	@Autowired private RepositorioUsuario servicoU;
+	
+	public Usuario salvar(Usuario u) {
+		
+		String hash = HashUtil.getSecureHash(u.getPassword());
+		u.setPassword(hash);
+		Usuario ucriado = servicoU.save(u);
+		return ucriado;
+	}
+	
+	public Usuario actualizar(Usuario u) {
+		
+		String hash = HashUtil.getSecureHash(u.getPassword());
+		u.setPassword(hash);
+		Usuario uactualizado = servicoU.save(u);
+		return uactualizado;
+	}
+	
+	public Usuario obterById(Long id) {
+		
+		Optional<Usuario> u = servicoU.findById(id);
+		return u.get();
+	}
+	
+	public List<Usuario> listarTodos() {
+		
+		List<Usuario> lista = servicoU.findAll();
+		return lista;
+	}
+	
+	public Usuario login(String email, String password) {
+		password = HashUtil.getSecureHash(password);
+		Optional<Usuario> u = servicoU.login(email, password);
+		return u.get();
+	}
+}
